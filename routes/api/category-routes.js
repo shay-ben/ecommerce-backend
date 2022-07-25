51ed438db8acfd5856f1e48421020b4be48f9a32
Your Name <you@example.com>
2022-07-25 17:48:00 -0400

    updated a category by its ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,19 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name,},
+      { where: {
+          id: req.params.id,
+        },
+      });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
