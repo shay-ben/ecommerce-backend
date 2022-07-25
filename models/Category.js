5b76a73e9bee30be455cd4585238dad1cc028ded
Your Name <you@example.com>
2022-07-25 16:21:55 -0400

    fixed category route bug and added category models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,15 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
