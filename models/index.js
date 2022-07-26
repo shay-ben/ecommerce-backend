9aa209d02f2390f56813587c60ea65d910578026
Your Name <you@example.com>
2022-07-25 21:15:20 -0400

    fix bug

--- models/index.js
+++ models/index.js
@@ -12,21 +12,21 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id"
+  foreignKey: "category_id"
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through:{
-    model: ProductTag
+    model: ProductTag,
   }
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag 
-  }
+    model: ProductTag,
+  },
 });
 
 module.exports = {
